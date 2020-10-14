89eaff107e6b6ad8025614f153172450def55587
Steve Rowley <srowley@gmail.com>
2020-10-13 20:29:00 -0500

    DRY legend protocol

--- lib/ggity/legend.ex
+++ lib/ggity/legend.ex
@@ -7,38 +7,22 @@ defprotocol GGity.Legend do
   def draw_legend(scale, label, key_glyph)
 end
 
-defimpl GGity.Legend, for: GGity.Scale.Alpha.Discrete do
-  def draw_legend(scale, label, key_glyph),
-    do: GGity.Scale.Alpha.Discrete.draw_legend(scale, label, key_glyph)
+defimpl GGity.Legend,
+  for: [
+    GGity.Scale.Alpha.Discrete,
+    GGity.Scale.Color.Viridis,
+    GGity.Scale.Fill.Viridis,
+    GGity.Scale.Linetype.Discrete,
+    GGity.Scale.Size.Discrete
+  ] do
+  def draw_legend(%scale_type{} = scale, label, key_glyph),
+    do: apply(scale_type, :draw_legend, [scale, label, key_glyph])
 end
 
-defimpl GGity.Legend, for: GGity.Scale.Color.Viridis do
-  def draw_legend(scale, label, key_glyph),
-    do: GGity.Scale.Color.Viridis.draw_legend(scale, label, key_glyph)
-end
-
-defimpl GGity.Legend, for: GGity.Scale.Fill.Viridis do
-  def draw_legend(scale, label, key_glyph),
-    do: GGity.Scale.Fill.Viridis.draw_legend(scale, label, key_glyph)
-end
-
-defimpl GGity.Legend, for: GGity.Scale.Linetype.Discrete do
-  def draw_legend(scale, label, key_glyph),
-    do: GGity.Scale.Linetype.Discrete.draw_legend(scale, label, key_glyph)
-end
-
-defimpl GGity.Legend, for: GGity.Scale.Shape do
-  def draw_legend(scale, label, _key_glyph), do: GGity.Scale.Shape.draw_legend(scale, label)
-end
-
-defimpl GGity.Legend, for: GGity.Scale.Shape.Manual do
-  def draw_legend(scale, label, _key_glyph),
-    do: GGity.Scale.Shape.Manual.draw_legend(scale, label)
-end
-
-defimpl GGity.Legend, for: GGity.Scale.Size.Discrete do
-  def draw_legend(scale, label, key_glyph),
-    do: GGity.Scale.Size.Discrete.draw_legend(scale, label, key_glyph)
+defimpl GGity.Legend, for: [GGity.Scale.Shape, GGity.Scale.Shape.Manual] do
+  def draw_legend(%scale_type{} = scale, label, _key_glyph) do
+    apply(scale_type, :draw_legend, [scale, label])
+  end
 end
 
 defimpl GGity.Legend, for: Any do
