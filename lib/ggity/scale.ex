377ea21a8a198132629f1d9bc25f7ea8a99d3807
Steve Rowley <srowley@gmail.com>
2020-10-23 16:55:47 -0500

    Consolidate scale implementations

--- lib/ggity/scale.ex
+++ lib/ggity/scale.ex
@@ -11,79 +11,33 @@ defprotocol GGity.Scale do
   def train(scale, parameters)
 end
 
-# TODO
-# Consolidate these, and add generic function for training with
-# just one data point
-defimpl GGity.Scale, for: GGity.Scale.Alpha.Continuous do
-  def train(scale, {min, max}),
-    do: GGity.Scale.Alpha.Continuous.train(scale, {min, max})
+defimpl GGity.Scale,
+  for: [
+    GGity.Scale.Alpha.Discrete,
+    GGity.Scale.Color.Viridis,
+    GGity.Scale.Fill.Viridis,
+    GGity.Scale.Identity,
+    GGity.Scale.Linetype.Discrete,
+    GGity.Scale.Shape.Manual,
+    GGity.Scale.Shape,
+    GGity.Scale.Size.Discrete,
+    GGity.Scale.X.Discrete
+  ] do
+  def train(%scale_type{} = scale, levels) do
+    apply(scale_type, :train, [scale, levels])
+  end
 end
 
-defimpl GGity.Scale, for: GGity.Scale.Alpha.Discrete do
-  def train(scale, levels),
-    do: GGity.Scale.Alpha.Discrete.train(scale, levels)
-end
-
-defimpl GGity.Scale, for: GGity.Scale.Color.Viridis do
-  def train(scale, levels),
-    do: GGity.Scale.Color.Viridis.train(scale, levels)
-end
-
-defimpl GGity.Scale, for: GGity.Scale.Fill.Viridis do
-  def train(scale, levels),
-    do: GGity.Scale.Fill.Viridis.train(scale, levels)
-end
-
-defimpl GGity.Scale, for: GGity.Scale.Identity do
-  def train(scale, levels),
-    do: GGity.Scale.Identity.train(scale, levels)
-end
-
-defimpl GGity.Scale, for: GGity.Scale.Linetype.Discrete do
-  def train(scale, levels),
-    do: GGity.Scale.Linetype.Discrete.train(scale, levels)
-end
-
-defimpl GGity.Scale, for: GGity.Scale.Shape do
-  def train(scale, levels), do: GGity.Scale.Shape.train(scale, levels)
-end
-
-defimpl GGity.Scale, for: GGity.Scale.Shape.Manual do
-  def train(scale, levels),
-    do: GGity.Scale.Shape.Manual.train(scale, levels)
-end
-
-defimpl GGity.Scale, for: GGity.Scale.Size.Continuous do
-  def train(scale, {min, max}),
-    do: GGity.Scale.Size.Continuous.train(scale, {min, max})
-end
-
-defimpl GGity.Scale, for: GGity.Scale.Size.Discrete do
-  def train(scale, levels),
-    do: GGity.Scale.Size.Discrete.train(scale, levels)
-end
-
-defimpl GGity.Scale, for: GGity.Scale.X.Continuous do
-  def train(scale, {min, max}),
-    do: GGity.Scale.X.Continuous.train(scale, {min, max})
-end
-
-defimpl GGity.Scale, for: GGity.Scale.X.Date do
-  def train(scale, {min, max}),
-    do: GGity.Scale.X.Date.train(scale, {min, max})
-end
-
-defimpl GGity.Scale, for: GGity.Scale.X.DateTime do
-  def train(scale, {min, max}),
-    do: GGity.Scale.X.DateTime.train(scale, {min, max})
-end
-
-defimpl GGity.Scale, for: GGity.Scale.X.Discrete do
-  def train(scale, levels),
-    do: GGity.Scale.X.Discrete.train(scale, levels)
-end
-
-defimpl GGity.Scale, for: GGity.Scale.Y.Continuous do
-  def train(scale, {min, max}),
-    do: GGity.Scale.Y.Continuous.train(scale, {min, max})
+defimpl GGity.Scale,
+  for: [
+    GGity.Scale.Alpha.Continuous,
+    GGity.Scale.Size.Continuous,
+    GGity.Scale.X.Continuous,
+    GGity.Scale.X.Date,
+    GGity.Scale.X.DateTime,
+    GGity.Scale.Y.Continuous
+  ] do
+  def train(%scale_type{} = scale, {min, max}) do
+    apply(scale_type, :train, [scale, {min, max}])
+  end
 end
