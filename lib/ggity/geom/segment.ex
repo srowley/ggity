5608f86cebb93792380e1c0b3b4f99bc74277f3d
Steve Rowley <srowley@gmail.com>
2022-03-21 19:23:54 -0500

    refactor segment drawing

--- lib/ggity/geom/segment.ex
+++ lib/ggity/geom/segment.ex
@@ -15,7 +15,7 @@ defmodule GGity.Geom.Segment do
             key_glyph: :line,
             alpha: 1,
             color: "black",
-            size: 0,
+            size: 1,
             custom_attributes: nil
 
   @spec new(mapping(), keyword()) :: Geom.Segment.t()
@@ -27,26 +27,15 @@ defmodule GGity.Geom.Segment do
   def draw(%Geom.Segment{} = geom_segment, data, plot), do: segments(geom_segment, data, plot)
 
   defp segments(%Geom.Segment{} = geom_segment, data, %Plot{scales: scales} = plot) do
-    scale_transforms =
-      geom_segment.mapping
-      |> Map.keys()
-      |> Enum.reduce(%{}, fn aesthetic, mapped ->
-        Map.put(mapped, aesthetic, Map.get(scales[aesthetic], :transform))
-      end)
+    scale_transforms = fetch_scale_transforms(geom_segment.mapping, scales)
+    fixed_aesthetics = fetch_fixed_aesthetics(geom_segment)
+    all_transforms = Map.merge(fixed_aesthetics, scale_transforms)
 
-    transforms =
-      geom_segment
-      |> Map.take([:alpha, :color, :size])
-      |> Enum.reduce(%{}, fn {aesthetic, fixed_value}, fixed ->
-        Map.put(fixed, aesthetic, fn _value -> fixed_value end)
-      end)
-      |> Map.merge(scale_transforms)
-
-    Enum.map(data, fn row -> segment(row, transforms, geom_segment, plot) end)
+    Enum.map(data, fn row -> segment(row, all_transforms, geom_segment, plot) end)
   end
 
   defp segment(row, transforms, geom_segment, plot) do
-    transformed_values = [
+    [x, xend, y, yend, stroke_opacity, stroke, stroke_width] = [
       transforms.x.(row[geom_segment.mapping.x]),
       transforms.x.(row[geom_segment.mapping.xend]),
       transforms.y.(row[geom_segment.mapping.y]),
@@ -56,20 +45,37 @@ defmodule GGity.Geom.Segment do
       transforms.size.(row[geom_segment.mapping[:size]])
     ]
 
-    labelled_values =
-      Enum.zip(
-        [:x, :xend, :y, :yend, :stroke_opacity, :stroke, :fill, :stroke_width],
-        transformed_values
-      )
-
-    options = Keyword.drop(labelled_values, [:x, :xend, :y, :yend])
-    x1 = labelled_values[:x] + plot.area_padding
-    x2 = labelled_values[:xend] + plot.area_padding
-    y1 = (plot.width - labelled_values[:y]) / plot.aspect_ratio + plot.area_padding
-    y2 = (plot.width - labelled_values[:yend]) / plot.aspect_ratio + plot.area_padding
+    x1 = x + plot.area_padding
+    x2 = xend + plot.area_padding
+    y1 = (plot.width - y) / plot.aspect_ratio + plot.area_padding
+    y2 = (plot.width - yend) / plot.aspect_ratio + plot.area_padding
 
     custom_attributes = GGity.Layer.custom_attributes(geom_segment, plot, row)
 
-    Draw.line([{:x1, x1}, {:x2, x2}, {:y1, y1}, {:y2, y2} | options] ++ custom_attributes)
+    [
+      x1: x1,
+      x2: x2,
+      y1: y1,
+      y2: y2,
+      stroke_opacity: stroke_opacity,
+      stroke: stroke,
+      stroke_width: stroke_width
+    ]
+    |> Keyword.merge(custom_attributes)
+    |> Draw.line()
+  end
+
+  defp fetch_scale_transforms(mapping, scales) do
+    for aes <- Map.keys(mapping), reduce: %{} do
+      scale_transforms -> Map.put(scale_transforms, aes, scales[aes].transform)
+    end
+  end
+
+  defp fetch_fixed_aesthetics(geom_segment) do
+    geom_segment
+    |> Map.take([:alpha, :color, :size])
+    |> Enum.reduce(%{}, fn {aesthetic, fixed_value}, fixed ->
+      Map.put(fixed, aesthetic, fn _value -> fixed_value end)
+    end)
   end
 end
