e8e564efa08f203376813d79e18797fcf44a444c
Steve Rowley <srowley@gmail.com>
2022-03-23 06:35:53 -0500

    refactor line drawing

--- lib/ggity/geom/line.ex
+++ lib/ggity/geom/line.ex
@@ -42,70 +42,78 @@ defmodule GGity.Geom.Line do
   @spec draw(Geom.Line.t(), list(map()), plot()) :: iolist()
   def draw(%Geom.Line{} = geom_line, _data, plot), do: lines(geom_line, plot)
 
-  defp lines(%Geom.Line{} = geom_line, plot) do
-    (geom_line.data || plot.data)
-    |> Enum.group_by(fn row ->
-      {
-        row[geom_line.mapping[:alpha]],
-        row[geom_line.mapping[:color]],
-        row[geom_line.mapping[:linetype]],
-        row[geom_line.mapping[:size]]
-      }
-    end)
-    |> Enum.map(fn {_value, group} -> line(geom_line, group, plot) end)
-  end
-
-  defp line(%Geom.Line{} = geom_line, data, %Plot{scales: scales} = plot) do
-    scale_transforms =
-      geom_line.mapping
-      |> Map.take([:x, :y, :color, :linetype, :size])
-      |> Map.keys()
-      |> Enum.reduce(%{}, fn aesthetic, mapped ->
-        Map.put(mapped, aesthetic, Map.get(scales[aesthetic], :transform))
-      end)
+  defp lines(%Geom.Line{} = geom_line, %Plot{} = plot) do
+    scale_transforms = scale_transforms_for(geom_line, plot.scales)
 
     transforms =
-      geom_line
-      |> Map.take([:alpha, :color, :linetype, :size])
-      |> Enum.reduce(%{}, fn {aesthetic, fixed_value}, fixed ->
-        Map.put(fixed, aesthetic, fn _value -> fixed_value end)
+      scale_transforms
+      |> Map.put(:x, fn row ->
+        transform_and_pad_x(row, scale_transforms.x, geom_line.mapping[:x], plot.area_padding)
       end)
-      |> Map.merge(scale_transforms)
+      |> Map.put(:y, fn row ->
+        transform_and_pad_y(
+          row,
+          scale_transforms.y,
+          geom_line.mapping[:y],
+          plot.area_padding,
+          plot.aspect_ratio,
+          plot.width
+        )
+      end)
 
+    (geom_line.data || plot.data)
+    |> Enum.group_by(fn row ->
+      %{
+        alpha: row[geom_line.mapping[:alpha]],
+        color: row[geom_line.mapping[:color]],
+        linetype: row[geom_line.mapping[:linetype]],
+        size: row[geom_line.mapping[:size]]
+      }
+    end)
+    |> Enum.map(fn {values, group} -> line(values, group, transforms) end)
+  end
+
+  defp scale_transforms_for(geom_line, scales) do
+    scale_transforms = fetch_scale_transforms(geom_line.mapping, scales)
+    fixed_aesthetics = fetch_fixed_aesthetics(geom_line)
+    Map.merge(fixed_aesthetics, scale_transforms)
+  end
+
+  defp fetch_scale_transforms(mapping, scales) do
+    for aes <- Map.keys(mapping), aes in [:x, :y, :color, :linetype, :size], reduce: %{} do
+      scale_transforms -> Map.put(scale_transforms, aes, scales[aes].transform)
+    end
+  end
+
+  defp fetch_fixed_aesthetics(geom_line) do
+    geom_line
+    |> Map.take([:alpha, :color, :linetype, :size])
+    |> Enum.reduce(%{}, fn {aesthetic, fixed_value}, fixed ->
+      Map.put(fixed, aesthetic, fn _value -> fixed_value end)
+    end)
+  end
+
+  defp transform_and_pad_x(row, x_transform, x_mapping, area_padding) do
+    x_transform.(row[x_mapping]) + area_padding
+  end
+
+  defp transform_and_pad_y(row, y_transform, y_mapping, area_padding, aspect_ratio, width) do
+    (width - y_transform.(row[y_mapping])) / aspect_ratio + area_padding
+  end
+
+  defp line(values, data, transforms) do
     coords =
       data
-      |> sort_by_x(geom_line)
-      |> Stream.map(fn row ->
-        [
-          transforms.x.(row[geom_line.mapping.x]),
-          transforms.y.(row[geom_line.mapping.y])
-        ]
-      end)
-      |> Stream.map(fn row -> Map.new(Enum.zip([:x, :y], row)) end)
-      |> Stream.map(fn row ->
-        Map.put(row, :y, (plot.width - row.y) / plot.aspect_ratio)
-      end)
-      |> Enum.map(fn row -> {row.x + plot.area_padding, row.y + plot.area_padding} end)
-
-    row = hd(data)
+      |> Enum.map(fn row -> {transforms.x.(row), transforms.y.(row)} end)
+      |> Enum.sort_by(fn {x, _y} -> x end)
 
     [alpha, color, linetype, size] = [
-      transforms.alpha.(row[geom_line.mapping[:alpha]]),
-      transforms.color.(row[geom_line.mapping[:color]]),
-      transforms.linetype.(row[geom_line.mapping[:linetype]]),
-      transforms.size.(row[geom_line.mapping[:size]])
+      transforms.alpha.(values.alpha),
+      transforms.color.(values.color),
+      transforms.linetype.(values.linetype),
+      transforms.size.(values.size)
     ]
 
     Draw.polyline(coords, color, size, alpha, linetype)
-  end
-
-  defp sort_by_x(data, %Geom.Line{} = geom_line) do
-    case hd(data)[geom_line.mapping.x] do
-      %Date{} ->
-        Enum.sort_by(data, fn row -> row[geom_line.mapping.x] end, Date)
-
-      _number ->
-        Enum.sort_by(data, fn row -> row[geom_line.mapping.x] end)
-    end
   end
 end
