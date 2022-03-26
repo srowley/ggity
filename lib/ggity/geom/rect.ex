d75bdaa21ff96d47cc4ba9e874795fc19df7fd85
Steve Rowley <srowley@gmail.com>
2022-03-26 10:17:58 -0500

    refactor rect drawing

--- lib/ggity/geom/rect.ex
+++ lib/ggity/geom/rect.ex
@@ -28,52 +28,46 @@ defmodule GGity.Geom.Rect do
   def draw(%Geom.Rect{} = geom_rect, data, plot), do: rects(geom_rect, data, plot)
 
   defp rects(%Geom.Rect{} = geom_rect, data, %Plot{scales: scales} = plot) do
-    scale_transforms =
-      geom_rect.mapping
-      |> Map.keys()
-      |> Enum.reduce(%{}, fn aesthetic, mapped ->
-        Map.put(mapped, aesthetic, Map.get(scales[aesthetic], :transform))
-      end)
-
-    transforms =
-      geom_rect
-      |> Map.take([:alpha, :color, :fill, :size])
-      |> Enum.reduce(%{}, fn {aesthetic, fixed_value}, fixed ->
-        Map.put(fixed, aesthetic, fn _value -> fixed_value end)
-      end)
-      |> Map.merge(scale_transforms)
+    scale_transforms = fetch_scale_transforms(geom_rect.mapping, scales)
+    fixed_aesthetics = fetch_fixed_aesthetics(geom_rect)
+    transforms = Map.merge(fixed_aesthetics, scale_transforms)
 
     Enum.map(data, fn row -> rect(row, transforms, geom_rect, plot) end)
   end
 
+  defp fetch_scale_transforms(mapping, scales) do
+    for aes <- Map.keys(mapping), reduce: %{} do
+      scale_transforms -> Map.put(scale_transforms, aes, scales[aes].transform)
+    end
+  end
+
+  defp fetch_fixed_aesthetics(geom_rect) do
+    geom_rect
+    |> Map.take([:alpha, :color, :fill, :size])
+    |> Enum.reduce(%{}, fn {aesthetic, fixed_value}, fixed ->
+      Map.put(fixed, aesthetic, fn _value -> fixed_value end)
+    end)
+  end
+
   defp rect(row, transforms, geom_rect, plot) do
-    transformed_values = [
-      transforms.x.(row[geom_rect.mapping.xmin]),
-      transforms.x.(row[geom_rect.mapping.xmax]),
-      transforms.y.(row[geom_rect.mapping.ymin]),
-      transforms.y.(row[geom_rect.mapping.ymax]),
-      transforms.alpha.(row[geom_rect.mapping[:alpha]]),
-      transforms.color.(row[geom_rect.mapping[:color]]),
-      transforms.fill.(row[geom_rect.mapping[:fill]]),
-      transforms.size.(row[geom_rect.mapping[:size]])
+    mapping = geom_rect.mapping
+
+    attributes = [
+      x: transforms.x.(row[mapping.xmin]) + plot.area_padding,
+      y: (plot.width - transforms.y.(row[mapping.ymax])) / plot.aspect_ratio + plot.area_padding,
+      height:
+        (transforms.y.(row[mapping.ymax]) - transforms.y.(row[geom_rect.mapping.ymin])) /
+          plot.aspect_ratio,
+      width: transforms.x.(row[mapping.xmax]) - transforms.x.(row[mapping.xmin]),
+      fill_opacity: transforms.alpha.(row[geom_rect.mapping[:alpha]]),
+      stroke: transforms.color.(row[geom_rect.mapping[:color]]),
+      fill: transforms.fill.(row[geom_rect.mapping[:fill]]),
+      stroke_width: transforms.size.(row[geom_rect.mapping[:size]])
     ]
 
-    labelled_values =
-      Enum.zip(
-        [:xmin, :xmax, :ymin, :ymax, :fill_opacity, :stroke, :fill, :stroke_width],
-        transformed_values
-      )
-
-    options = Keyword.drop(labelled_values, [:xmin, :xmax, :ymin, :ymax])
-    x = labelled_values[:xmin] + plot.area_padding
-    y = (plot.width - labelled_values[:ymax]) / plot.aspect_ratio + plot.area_padding
-    height = (labelled_values[:ymax] - labelled_values[:ymin]) / plot.aspect_ratio
-    width = labelled_values[:xmax] - labelled_values[:xmin]
-
-    custom_attributes = GGity.Layer.custom_attributes(geom_rect, plot, row)
-
-    Draw.rect(
-      [{:x, x}, {:y, y}, {:height, height}, {:width, width} | options] ++ custom_attributes
-    )
+    geom_rect
+    |> GGity.Layer.custom_attributes(plot, row)
+    |> Keyword.merge(attributes)
+    |> Draw.rect()
   end
 end
